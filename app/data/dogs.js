9f06e0c8969bf0a43a61811a1eff4b2df6d931c4
Matt White <mattwhite614@gmail.com>
2016-07-23 16:45:08 -0500

    rdy for turn in

--- app/data/dogs.js
+++ app/data/dogs.js
@@ -1,24 +1,26 @@
 exports.dogs = [
 	{
-		"breed" : 'husky',
+		"breed" : 'Husky',
 		"imgURL" : 'http://cdn1-www.dogtime.com/assets/uploads/gallery/siberian-husky-dog-breed-pictures/siberian-husky-dog-breed-pictures-1.jpg',
 		"scores" : [
-      1,
+      5,
       2,
       3,
-      4,
+      1,
       5,
       ],
+			"total": 16
     },
 		{
-			"breed" : 'corgi',
+			"breed" : 'Corgi',
 			"imgURL" : 'https://i.ytimg.com/vi/oVXZTmi2ruI/maxresdefault.jpg',
 			"scores" : [
 	      1,
 	      2,
+	      2,
 	      3,
-	      4,
 	      5,
 			],
+			"total": 13
 		}
   ];
