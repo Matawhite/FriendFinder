b1c1b45154b223b0156637000158df18964cf721
Matt White <mattwhite614@gmail.com>
2016-07-23 12:39:50 -0500

    changed ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var PORT = 3000;
+var PORT = 5000;
 var app = express();
 
 app.use(bodyParser.json());
