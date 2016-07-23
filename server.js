1d2fe0443a7eaf6d64e5458793c2eb5518538eca
Matt White <mattwhite614@gmail.com>
2016-07-23 12:27:09 -0500

    test deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var PORT = 8080;
+var PORT = 3000;
 var app = express();
 
 app.use(bodyParser.json());
